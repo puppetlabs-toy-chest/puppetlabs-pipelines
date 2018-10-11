1ca752d62f703c41450542f42a6a2c2d3067819c
Brian Maher <brian.maher@puppet.com>
2018-10-11 16:30:51 -0700

    WIP: fixes

--- manifests/agent/windows.pp
+++ manifests/agent/windows.pp
@@ -4,25 +4,25 @@
 class pipelines::agent::windows {
   $env = $facts['pipelines_env']
 
-  if ! $puppet::agent::install_dir {
+  if ! $pipelines::agent::install_dir {
     $install_dir = "${env['ProgramFiles']}\\Distelli"
   } else {
-    $install_dir = $puppet::agent::install_dir
+    $install_dir = $pipelines::agent::install_dir
   }
 
-  if $puppet::agent::version {
-    $download_url = "${puppet::agent::download_url}/${puppet::agent::version}.ps1"
+  if $pipelines::agent::version {
+    $download_url = "${pipelines::agent::download_url}/${pipelines::agent::version}.ps1"
   } else {
-    $download_url = "${puppet::agent::download_url}.ps1"
+    $download_url = "${pipelines::agent::download_url}.ps1"
   }
-  $download_location = "${install_dir}/distelli-download.ps1"
-  $download_cmd = "cmd.exe /c \"powershell -NoProfile -ExecutionPolicy Bypass -File \"${download_location}\"\""
+  $download_location = "${install_dir}\\distelli-download.ps1"
+  $download_cmd = "cmd.exe /c \"powershell -NoProfile -ExecutionPolicy Bypass -Command - < \"${download_location}\"\""
   $agent_conf_file = "${env['SystemDrive']}\\distelli.yml"
 
   exec { "mkdir ${install_dir}":
     command => "cmd.exe /c \"md \"${install_dir}\"\"",
     path    => $facts['path'],
-    onlyif  => "cmd.exe /c \"if exist \"${install_dir}\" exit 1",
+    onlyif  => "cmd.exe /c \"if exist \"${install_dir}\" exit 1\"",
   }
   file { $download_location:
     source  => $download_url,
@@ -30,7 +30,7 @@ class pipelines::agent::windows {
   }
   exec { 'pipelines::agent download':
     require     => Exec["mkdir ${install_dir}"],
-    creates     => "${install_dir}/distelli.exe",
+    creates     => "${install_dir}\\distelli.exe",
     path        => $facts['path'],
     environment => [
       "DISTELLI_INSTALL_DIR=${install_dir}",
@@ -50,9 +50,9 @@ class pipelines::agent::windows {
       show_diff => false,
     }
     if $pipelines::agent::data_dir {
-      $install_cmd = "cmd.exe /c \"distelli agent -data-dir \"${pipelines::agent::data_dir}\" install -readyml\" <nil"
+      $install_cmd = "cmd.exe /c \"distelli agent -data-dir \"${pipelines::agent::data_dir}\" install -readyml <nil\""
     } else {
-      $install_cmd = "cmd.exe /c \"distelli agent install -readyml\" <nil"
+      $install_cmd = "cmd.exe /c \"distelli agent install -readyml <nil\""
     }
     exec { 'pipelines::agent install':
       command     => $install_cmd,
